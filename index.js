0e4169b9d27a3854c68f947861360cf8afa3377e
synamatics <synamatics@gmail.com>
2019-05-27 11:03:18 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+var axios = require('axios');
+var port = process.env.PORT || 3000;
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(port, function(){
+  console.log('Application Started at '+port+' PORT');
+});
+
+let socketList = io.sockets.server.eio.clients;
+let clientsCount = 0;
+io.on('connection', function(socket){
+    clientsCount++;
+    socket.on('incoming_message', function(message){
+        console.log(clientsCount)
+        if(clientsCount > 1){
+            io.emit('receive_message',message);
+        }
+        else{
+            axios.post("http://localhost:8000/send/email",message)
+            .then(res=>{
+                console.log("Email Notified")
+                console.log(res.data)
+            })
+            .catch(error=>{
+                console.log(error)
+                console.log("Unable to Send Email Notfication")
+            })
+        }
+    });
+    
+    socket.on('disconnect', function(){
+        clientsCount--;
+      console.log('user disconnected');
+    });
+  });
