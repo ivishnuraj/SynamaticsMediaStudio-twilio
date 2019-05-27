c638485fbfe8f125162577ce374f094f1cd820f5
synamatics <synamatics@gmail.com>
2019-05-27 13:36:11 +0530

    Updates

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var app = require('express')();
 var http = require('http').createServer(app);
-const io = require("socket.io")(server, {
+const io = require("socket.io")(http, {
     handlePreflightRequest: (req, res) => {
         const headers = {
-            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+            "Access-Control-Allow-Headers": "*",
             "Access-Control-Allow-Origin": req.headers.origin,
         };
         res.writeHead(200, headers);
